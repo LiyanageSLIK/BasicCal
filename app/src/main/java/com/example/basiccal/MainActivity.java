4d09548fc935ca9c53edefa173cb7ab75d23cc2e
IsuruKwashalya <150815b@uom.lk>
2022-12-19 12:54:12 +0530

    basic number press method

--- app/src/main/java/com/example/basiccal/MainActivity.java
+++ app/src/main/java/com/example/basiccal/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.basiccal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    TextView currentTextView ;
+    volatile String  currentText;
+   
+
+
+
+
+
+
+
+
+
+
+
+    public void numberKeyPress(View view){
+        currentTextView= findViewById(R.id.textView);
+        currentText =currentTextView.getText().toString();
+
+            switch (view.getId()){
+                case R.id.btn_0:
+                    if (!currentText.equals("0")){
+                        currentTextView.append("0");
+                    }else {currentTextView.setText("0");}
+
+                    break;
+                case R.id.btn_plusminus:
+                    if (Float.valueOf(currentText)>0){
+                        currentTextView.setText("-"+currentText);
+                    }
+                    if (Float.valueOf(currentText)<0){
+                        currentTextView.setText(currentText.substring(1));
+                    }
+
+                    break;
+                case R.id.btn_dot:
+                    if(currentText.contains(".")) {
+                        currentTextView.append(".");
+                    }
+
+                    break;
+                default:
+                    if (currentText.equals("0")){
+                        currentTextView.setText(view.getTag().toString());
+                    }
+                    else {
+                        currentTextView.append(view.getTag().toString());
+                    }
+
+                    break;
+
+            }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
